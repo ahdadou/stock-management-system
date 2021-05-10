73a59ddbee94dcca0d20bd6f4d9c2b1d972151cf
mohamedaghzaou <mohamed-aghzaou@hotmail.com>
2021-05-10 17:27:26 +0000

    add code to SupplierProduct (controller & service)

--- src/main/java/org/sic/Projet_GestionDesStock/services/SipplierService.java
+++ src/main/java/org/sic/Projet_GestionDesStock/services/SipplierService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class SipplierService {
 
 //    Add Item
-	public Supplier saveItem(Supplier category) {
+	public Supplier saveItem(Supplier supplier) {
 		return null;
 	}
 
